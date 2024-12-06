99d053f2f87662a4ac3446a9101b201cdb85192e
jacksonfixsen <jacksonfixsen@gmail.com>
2024-12-06 11:11:39 -0500

    Update STAT383 PROJECT.R
    Finished code

--- STAT383 PROJECT.R
+++ STAT383 PROJECT.R
@@ -1,38 +1,62 @@
 graphics.off() # clear all previous plots
 rm(list=ls()) # clear the environment from previous codes
 cat("\014") # clear the console 
-
-
+setwd("C:/Users/User/Desktop/Voting_Project_Stats")# set working directory
 library(readxl)
-df <- read_excel("consolidated_data.xlsx") 
+df <- read_excel("consolidated_data.xlsx") #read from excel file
 View(df)
 head(df)
 
 # change a column name
 
 colnames(df)[3] <- "different"
+
+#Total Trump Votes
 sum(df$vote=="Donald Trump")
-sum(df$vote=="Kamala Harris")
-sum(df$vote=="Other")
+
+
+#total voters
 nrow(df)
 
-# proportion of Donald Trump voters
-print(sum(df$vote=="Donald Trump")/nrow(df))
-
-# proportion of Kamala Harris voters
-print(sum(df$vote=="Kamala Harris")/nrow(df))
-
-# proportion of Other voters
-print(sum(df$vote=="Other")/nrow(df))
-
-# Subsetting a dataset
-# 
+# number of males
 df_males <- df[df$gender==1,]
 
-df_females <- df[df$gender==0,]
 
 # proportion of Donald Trump voters amongst male students
-print(sum(df_males$vote=="Donald Trump")/nrow(df_males))
+Trump_Male_Voters <- sum(df_males$vote == "Donald Trump") / nrow(df_males)
+print(Trump_Male_Voters)
+Actual_Trump_Voters <- .49
 
-# proportion of Donald Trump voters amongst female students
-print(sum(df_females$vote=="Donald Trump")/nrow(df_females))
+
+# Significance level
+alpha <- 0.05
+
+# Hypothesized proportion (p0)
+p0 <- Actual_Trump_Voters  
+
+# Sample size for males
+n_males <- as.numeric(nrow(df_males))
+
+# Proportion of male Trump voters observed
+p_hat <- Trump_Male_Voters
+
+# Standard error of the proportion
+se <- sqrt(p0 * (1 - p0) / n_males)
+
+# Critical z-value for two-tailed test
+z_critical <- qnorm(1 - alpha / 2)
+
+# Compute the test statistic (z-score)
+TS <- (p_hat - p0) / se
+
+infinity <- "\u221e"
+
+# Rejection region
+cat("Rejection Region: (-", infinity, ",", -z_critical,"]U[", z_critical,",",infinity, ")", "\n")
+
+
+# Rejection decision
+reject_null <- abs(TS) < z_critical
+
+#Print decision
+cat("Reject Null Hypothesis: ", reject_null, "\n")
